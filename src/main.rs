d04749f3448843c2c1b889760ea8722c0003809f
Dylan Jenik <dylanjenikcodes@gmail.com>
2023-10-14 09:46:13 -0400

    style: directly import run_advent_day in main.rs from lib.rs instead of calling it using namespace

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
 use std::process;
 
-use rust_advent;
-use rust_advent::CLIParams;
+use rust_advent::{run_advent_day, CLIParams};
 
 fn main() {
     let cli_params = CLIParams::build(env::args()).unwrap_or_else(|err| {
@@ -10,5 +9,5 @@ fn main() {
         process::exit(1);
     });
 
-    rust_advent::run_advent_day(&cli_params);
+    run_advent_day(&cli_params);
 }
